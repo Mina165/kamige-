ef44356cc1ceeeb3f8b95be50d5c07a02de08d4a
BuildTools <unconfigured@null.spigotmc.org>
2022-09-30 20:21:42 +0900

    first commit

---
+++ src/World/World.cc
@@ -0,0 +1,42 @@
+#include "World.hpp"
+World* World::instancePtr=nullptr;
+
+World::~World()
+{
+    instancePtr=nullptr;
+}
+void World::GenerateWorld()
+{
+    for(int i=0;i<GetXSize();i++)
+    {
+        for(int j=GetYSize()/4 ; j>GetYSize() ; j++)
+        {
+            worldData.at(i).at(j)=BlockData(BlockData::BlockType::grass);
+        }
+    }
+}
+int World::GetXSize()
+{
+    return static_cast<int>(worldData.size());
+}
+int World::GetYSize()
+{
+    return static_cast<int>(worldData.at(0).size());
+}
+BlockData* World::GetBlockData(int x,int y)
+{
+    return &(worldData.at(x).at(y));
+}
+void World::DisplayWorld()
+{
+    for(int i=0;i<GetXSize();i++)
+    {
+        for(int j=0;j<GetYSize();j++){
+            if(worldData.at(i).at(j).type==BlockData::BlockType::air){
+                std::cout<<"a";
+            }
+
+        }
+    }
+}
+
---
+++ src/World/World.hpp
@@ -0,0 +1,27 @@
+#pragma once
+#include <vector>
+#include <iostream>
+#include <cassert>
+#include "BlockData.hpp"
+class World
+{
+public:
+    World(int initalXSize, int initalYSize):
+        worldData(initalXSize,std::vector<BlockData>(initalYSize,BlockData(BlockData::BlockType::air)))
+        {
+            assert(instancePtr == nullptr);
+            instancePtr=this;
+            GenerateWorld();
+            DisplayWorld();
+        }
+    ~World();
+    int GetXSize();
+    int GetYSize();
+    BlockData* GetBlockData(int x,int y);
+    void DisplayWorld();
+    static World* GetInstance(){return instancePtr;};
+private:
+    static World* instancePtr;
+    std::vector<std::vector<BlockData>> worldData;
+    void GenerateWorld();
+};
