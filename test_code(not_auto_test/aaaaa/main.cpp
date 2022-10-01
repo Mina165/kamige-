bdd49c7817bb30a0f0213e57445e8742db50e4b3
Mina165 <110606833+Mina165@users.noreply.github.com>
2022-10-01 11:44:51 +0900

    Update main.cpp

--- test_code(not_auto_test/aaaaa/main.cpp
+++ test_code(not_auto_test/aaaaa/main.cpp
@@ -6,16 +6,19 @@ struct Aiueo{
 };
 int main(int argc, char **argv){
   std::vector<std::vector<Aiueo>> data(10,std::vector<Aiueo>(10,Aiueo(0)));
+  
   for(int i=0;i<data.size();i++){
     for(int j=0;j<data.at(0).size();j++){
       std::cout<<data.at(i).at(j).a<<std::endl;
     }
   }
+  
   for(int i=0;i<data.size();i++){
     for(int j=0;j<data.at(0).size();j++){
       data.at(i).at(j)=Aiueo(50);
     }
   }
+  
   for(int i=0;i<data.size();i++){
     for(int j=0;j<data.at(0).size();j++){
       std::cout<<data.at(i).at(j).a<<std::endl;
